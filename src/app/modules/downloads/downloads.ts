504b101dbec4f25727e6a27c5b8aa51b6d339869
Kanyango <kariukikanyango@gmail.com>
2018-05-31 19:42:22 +0300

    Update downloads.ts

--- src/app/modules/downloads/downloads.ts
+++ src/app/modules/downloads/downloads.ts
@@ -1,1 +1,11 @@
 
+import { Component } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+
+@Component({
+    selector: 'downloads-component',
+    templateUrl: './downloads.html'
+})
+
+export class DownloadsComponent {
+}
