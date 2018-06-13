6865304555c484387110e156f6011433ec3d7997
Kanyango <kariukikanyango@gmail.com>
2018-06-13 10:30:32 +0300

    Update gallery.ts

--- src/app/modules/gallery/gallery.ts
+++ src/app/modules/gallery/gallery.ts
@@ -15,12 +15,12 @@ export class GalleryComponent implements OnInit
 
     this.images =
                   [
-                  {img: "/../../../assets/gal_28.JPG"},
-                  {img: "/../../../assets/gal_4.JPG"},
+                  //{img: "/../../../assets/gal_28.JPG"},
+                  //{img: "/../../../assets/gal_4.JPG"},
                   {img: "/../../../assets/gal_16.JPG"},
                   {img: "/../../../assets/gal_18.JPG"},
-                  {img: "/../../../assets/gal_1.JPG"},
-                  {img: "/../../../assets/gal_2.JPG"},
+                  //{img: "/../../../assets/gal_1.JPG"},
+                  //{img: "/../../../assets/gal_2.JPG"},
                   {img: "/../../../assets/gal_5.JPG"},
                   {img: "/../../../assets/gal_6.JPG"},
                   {img: "/../../../assets/gal_7.JPG"},
@@ -31,16 +31,14 @@ export class GalleryComponent implements OnInit
                   {img: "/../../../assets/gal_13.JPG"},
                   {img: "/../../../assets/gal_14.JPG"},
                   {img: "/../../../assets/gal_15.JPG"},
-
                   {img: "/../../../assets/gal_17.JPG"},
-
                   {img: "/../../../assets/gal_19.JPG"},
                   {img: "/../../../assets/gal_20.JPG"},
                   {img: "/../../../assets/gal_21.JPG"},
                   {img: "/../../../assets/gal_22.JPG"},
                   {img: "/../../../assets/gal_23.JPG"},
                   {img: "/../../../assets/gal_24.JPG"},
-                  {img: "/../../../assets/gal_25.JPG"},
+                  //{img: "/../../../assets/gal_25.JPG"},
                   {img: "/../../../assets/gal_26.JPG"},
                   {img: "/../../../assets/gal_27.JPG"},
                   {img: "/../../../assets/gal_28.JPG"},
